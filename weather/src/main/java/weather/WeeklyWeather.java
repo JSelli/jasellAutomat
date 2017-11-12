f9eb803598164d309cce20344d7f60cbd3da3837
Jaagup <jasell@ttu.ee>
2017-11-12 22:52:17 +0200

    1asd

--- weather/src/main/java/weather/WeeklyWeather.java
+++ weather/src/main/java/weather/WeeklyWeather.java
@@ -1,11 +1,38 @@
 package weather;
 
+import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
+import reader.ReadAPI;
+
+import java.io.IOException;
 
 public class WeeklyWeather implements WeatherRequest {
+
+    private JSONObject jsonObject;
+
+    public WeeklyWeather() throws IOException, JSONException {
+        ReadAPI reader = new ReadAPI();
+        jsonObject = reader.getJSONNameForecast("Tallinn");
+    }
+
+    public WeeklyWeather(String name) throws IOException, JSONException {
+        ReadAPI reader = new ReadAPI();
+        jsonObject = reader.getJSONNameForecast(name);
+    }
+
     @Override
-    public Object getWeather() {
-        return null;
+    public Object getWeather() throws JSONException {
+        JSONArray object = (JSONArray) jsonObject.get("list");
+
+        for (int i = 0; i < 40; i++) {
+            JSONObject jO = new JSONObject(object.get(i).toString());
+            System.out.println(jO.get("main"));
+        }
+
+
+
+        return object;
     }
 
     @Override
